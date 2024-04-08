71ae6099f7d997410b88f817e0d93bef9aaa96bb
Юля <topolya.krutaya@gmail.com>
2024-04-08 00:48:59 -0700

    refresh composer

--- pop-it-mvc/core/bootstrap.php
+++ pop-it-mvc/core/bootstrap.php
@@ -3,7 +3,7 @@
 const DIR_CONFIG = '/../config';
 
 //Подключение автозагрузчика composer
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
 
 //Функция, возвращающая массив всех настроек приложения
 function getConfigs(string $path = DIR_CONFIG): array
