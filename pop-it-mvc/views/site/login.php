181e2e46c2565b5fb9e4f57c2506c625a2efb9a1
Корнилова Юля <topolya.krutaya@gmail.com>
2024-04-10 19:15:42 +0700

    Верстка

--- pop-it-mvc/views/site/login.php
+++ pop-it-mvc/views/site/login.php
@@ -1,4 +1,15 @@
-<h2>Авторизация</h2>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel ="stylesheet" href="giyetff-m3/pop-it-mvc/public/css/style.css">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pop it MVC</title>
+</head>
+<h1>Авторизация</h1>
+<h2>Библиотека</h2>
 <h3><?= $message ?? ''; ?></h3>
 
 <h3><?= app()->auth->user()->name ?? ''; ?></h3>
