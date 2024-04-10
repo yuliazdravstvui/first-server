9800d3007034ae2903598f95b22e5b927a3a142e
Корнилова Юля <topolya.krutaya@gmail.com>
2024-04-10 16:51:49 +0700

    Работа с сессиями и редиректами

--- pop-it-mvc/core/bootstrap.php
+++ pop-it-mvc/core/bootstrap.php
@@ -5,6 +5,17 @@ const DIR_CONFIG = '/../config';
 //Подключение автозагрузчика composer
 require_once __DIR__ . '/../../vendor/autoload.php';
 
+require_once __DIR__ . '/../routes/web.php';
+$app = new Src\Application(new Src\Settings(getConfigs()));
+
+//Функция возвращает глобальный экземпляр приложения
+function app() {
+    global $app;
+    return $app;
+}
+
+return $app;
+
 //Функция, возвращающая массив всех настроек приложения
 function getConfigs(string $path = DIR_CONFIG): array
 {
