1c1095d3284119aff26a6d4638b0c86569d2cb73
Корнилова Юля <topolya.krutaya@gmail.com>
2024-04-11 17:08:49 +0700

    верчтка авторизации

--- pop-it-mvc/views/layouts/main.php
+++ pop-it-mvc/views/layouts/main.php
@@ -4,13 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <link rel ="stylesheet" href="/pop-it-mvc/public/css/style.css">
+    <link rel ="stylesheet" href="/pop-it-mvc/public/css/nav.css">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Pop it MVC</title>
 </head>
 <body>
 <header>
     <nav>
+        <div class="head">
+        <img src="img/логотип.svg">
+        <img src="img/Библиотека.svg">
+        </div>
 <!--        <a href="--><?php //= app()->route->getUrl('/hello') ?><!--">Главная</a>-->
         <?php
         if (!app()->auth::check()):
--- pop-it-mvc/views/site/login.php
+++ pop-it-mvc/views/site/login.php
@@ -1,24 +1,24 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <link rel ="stylesheet" href="giyetff-m3/pop-it-mvc/public/css/style.css">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Pop it MVC</title>
-</head>
-<h1>Авторизация</h1>
-<h2>Библиотека</h2>
-<h3><?= $message ?? ''; ?></h3>
+<link rel ="stylesheet" href="/pop-it-mvc/public/css/login.css">
+    <h1>Авторизация</h1>
 
-<h3><?= app()->auth->user()->name ?? ''; ?></h3>
-<?php
-if (!app()->auth::check()):
+<div class="log">
+    <h3><?= $message ?? ''; ?></h3>
+
+    <h3><?= app()->auth->user()->name ?? ''; ?></h3>
+    <?php
+    if (!app()->auth::check()):
     ?>
+
     <form method="post">
-        <label>Логин <input type="text" name="login"></label>
-        <label>Пароль <input type="password" name="password"></label>
-        <button>Войти</button>
+        <img class="logo" src="img/logo.svg"
+        <h2>Библиотека</h2>
+        <div class="login">
+            Логин <input type="text" name="login">
+            Пароль <input type="password" name="password">
+            <button>Войти</button>
+        </div>
+
     </form>
+</div>
+
 <?php endif;
