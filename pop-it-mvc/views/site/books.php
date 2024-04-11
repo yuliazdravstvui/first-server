bf2e0e71498062b494191ce033add212cdc78d77
Корнилова Юля <topolya.krutaya@gmail.com>
2024-04-11 19:22:38 +0700

    Верстка добавление нового читателя дубль 2

---
+++ pop-it-mvc/views/site/books.php
@@ -0,0 +1,39 @@
+<link rel ="stylesheet" href="/pop-it-mvc/public/css/books.css">
+<h1>Книги</h1>
+<div class="books">
+    <div class="book">
+        <div class="info">
+            <div class="name">
+                <h3>Война и мир</h3>
+                <p>1493 ₽</p>
+            </div>
+            <p>1867 г.</p>
+            <p>Толстой Лев Николаевич</p>
+        </div>
+        <button type="submit">Подробнее</button>
+    </div>
+    <div class="book">
+        <div class="info">
+            <div class="name">
+                <h3>Война и мир</h3>
+                <p>1493 ₽</p>
+            </div>
+            <p>1867 г.</p>
+            <p>Толстой Лев Николаевич</p>
+        </div>
+        <button type="submit">Подробнее</button>
+    </div>
+    <div class="book">
+        <div class="info">
+            <div class="name">
+                <h3>Война и мир</h3>
+                <p>1493 ₽</p>
+            </div>
+            <p>1867 г.</p>
+            <p>Толстой Лев Николаевич</p>
+        </div>
+        <button type="submit">Подробнее</button>
+    </div>
+</div>
+<button type="submit">Выдать книгу читателю</button>
+
