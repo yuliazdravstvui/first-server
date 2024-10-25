e3a040420474ec0cdbe324d4ddc1af7791b3bd1f
yuliazdravstvui <topolya.krutaya@gmail.com>
2024-10-25 17:24:50 +0700

    немного изменен

--- pop-it-mvc/config/db.php
+++ pop-it-mvc/config/db.php
@@ -2,9 +2,9 @@
 return [
     'driver' => 'mysql',
     'host' => 'localhost',
-    'database' => 'fiyscais_m3',
-    'username' => 'fiyscais',
-    'password' => '8tHtRi',
+    'database' => 'mvc',
+    'username' => 'root',
+    'password' => '',
     'charset' => 'utf8',
     'collation' => 'utf8_unicode_ci',
     'prefix' => '',
